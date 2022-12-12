859bef530f57e47097e588d0dc7177fe2e7b7c7f
Vincent--Li <q315744068@gmail.com>
2022-12-12 21:25:49 +0800

    UPDATE:

---
+++ lab/craft/SimpleLexer.java
@@ -0,0 +1,14 @@
+import java.io.CharArrayReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleLexer {
+  public static void main(String args[]) {
+    SimpleLexer lexer = new SimpleLexer();
+
+    String script = "int age = 45;";
+    System.out.println()
+  }
+
+}
